597a3da9e9ae30ff2c1f8c183b24c55d46122fa5
Piotr Suwała <psuwala@atlassian.com>
2020-09-03 17:23:10 +0200

    implement: toTree creates representation for non empty root

--- src/utils/ToTree.ts
+++ src/utils/ToTree.ts
@@ -1,3 +1,4 @@
-export function toTree(obj : object){
-  return {params:{}, value:{}};
+export function toTree(obj: object) {
+  const entries = Object.entries(obj).map(([id, value]) => ({ params: { id }, value }));
+  return { params: {}, value: entries };
 }

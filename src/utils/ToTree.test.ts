42071c0a94ddfdfe5c5e130cb803c3c3b2c229f7
Piotr Suwała <psuwala@atlassian.com>
2020-09-03 17:20:01 +0200

    test: toTree creates representation for non empty root

--- src/utils/ToTree.test.ts
+++ src/utils/ToTree.test.ts
@@ -1,5 +1,9 @@
 import { toTree } from "./ToTree";
 
 test("toTree creates root representation", ()=>{
-   expect(toTree({})).toEqual({params:{}, value:{}});
+   expect(toTree({})).toEqual({params:{}, value:[]});
+});
+
+test("toTree creates representation for non empty root", ()=>{
+   expect(toTree({ksi: 'delta'})).toEqual({params:{}, value:[{params:{id: 'ksi'}, value:'delta'}]});
 });

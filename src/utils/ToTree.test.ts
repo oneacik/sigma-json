85353269b2a8e6cbb5c39b68113f4940698894dc
Piotr Suwała <psuwala@atlassian.com>
2020-09-03 17:29:54 +0200

    refactor: toTree creates representation for non empty root

--- src/utils/ToTree.test.ts
+++ src/utils/ToTree.test.ts
@@ -1,9 +1,21 @@
 import { toTree } from "./ToTree";
 
-test("toTree creates root representation", ()=>{
-   expect(toTree({})).toEqual({params:{}, value:[]});
-});
+function $(id, value) {
+  return { params: { id }, value };
+}
 
-test("toTree creates representation for non empty root", ()=>{
-   expect(toTree({ksi: 'delta'})).toEqual({params:{}, value:[{params:{id: 'ksi'}, value:'delta'}]});
+describe('full', () => {
+  test("toTree creates root representation", () => {
+    expect(toTree({}))
+      .toEqual($("root", []));
+  });
+
+  test("toTree creates representation for non empty root", () => {
+    expect(toTree({ ksi: "delta" }))
+      .toEqual(
+        $("root", [
+          $("ksi", "delta")
+        ]));
+  });
+
 });
--- src/utils/ToTree.ts
+++ src/utils/ToTree.ts
@@ -1,4 +1,4 @@
 export function toTree(obj: object) {
   const entries = Object.entries(obj).map(([id, value]) => ({ params: { id }, value }));
-  return { params: {}, value: entries };
+  return { params: {id:"root"}, value: entries };
 }

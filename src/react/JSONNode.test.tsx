ad102aa5b60e6de8136525cce11dc3b18878ed71
Piotr Suwała <psuwala@atlassian.com>
2020-09-03 19:24:40 +0200

    refactor: Root Json Node

--- src/react/JSONNode.test.tsx
+++ src/react/JSONNode.test.tsx
@@ -1,26 +1,18 @@
 import { shallow, configure } from "enzyme";
 import { JSONNode } from "./JSONNode";
-import { Node } from "../utils/ToTree";
 import * as React from "react";
 import Adapter from "enzyme-adapter-react-16";
 
-
 configure({ adapter: new Adapter() });
-
-const sample: Node = {
-  params: { id: "root" },
-  value: [
-    { params: { id: "ksi" }, value: "delta" },
-    { params: { id: "de" }, value: "mo" }
-  ]
-};
 
 test("Root Json Node", () => {
   const sample = {
     params: { id: "root" }, value: []
   };
-  const render: any = shallow(<JSONNode node={sample}></JSONNode>);
-  render.equals(<div>
-    <div>root</div>
-  </div>);
+  const render: any = shallow(<JSONNode node={sample}/>);
+  render.equals(
+    <div>
+      <div>root</div>
+    </div>
+  );
 });
--- src/react/JSONNode.tsx
+++ src/react/JSONNode.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
-import { ClassAttributes, Props, ReactPropTypes } from "react";
 import { Node } from "../utils/ToTree";
 
 export function JSONNode(props: JSONNodeProps) {
-  return <div>
-    <div>{props.node.params.id}</div>
-  </div>;
-
+  return (
+    <div>
+      <div>{props.node.params.id}</div>
+    </div>
+  );
 }
 
 interface JSONNodeProps {
   node: Node
-};
-;
+}
+

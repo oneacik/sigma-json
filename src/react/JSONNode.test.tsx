8cc2561d84abafa03b5997325b44d02d315fd60c
Piotr Suwała <psuwala@atlassian.com>
2020-09-05 20:17:38 +0200

    test/impl: Root Json Expands On Click

--- src/react/JSONNode.test.tsx
+++ src/react/JSONNode.test.tsx
@@ -1,23 +1,32 @@
-import { shallow, configure } from "enzyme";
+import { shallow, configure, mount } from "enzyme";
 import { JSONNode } from "./JSONNode";
 import * as React from "react";
 import Adapter from "enzyme-adapter-react-16";
 import { $ } from "../utils/ToTree.test";
+import { observable } from "mobx";
+import "jsdom-global/register";
 
 configure({ adapter: new Adapter() });
 
 describe("Root Json Tests", () => {
   test("Root Json Node", () => {
-    const sample = $("root", []);
+    const sample = observable($("root", []));
     const render = shallow(<JSONNode node={sample}/>);
-    expect(render.find('div div.name').text()).toEqual("root");
+    expect(render.find("div div.name").text()).toEqual("root");
   });
 
   test("Root Json Modifies Expanded On Click", () => {
-    const sample = $("root", []);
+    const sample = observable($("root", []));
     const render = shallow(<JSONNode node={sample}/>);
     render.find(".expander").simulate("click");
     expect(sample.params.expanded).toBeTruthy();
+  });
+
+  test("Root Json Expands On Click", () => {
+    const sample = observable($("root", [$("door", "knob", false)]));
+    const render = mount(<JSONNode node={sample}/>);
+    render.find("div > .elements .expander").simulate("click");
+    expect(render.find(".elements .name").text()).toEqual("door");
   });
 });
 
--- src/react/JSONNode.tsx
+++ src/react/JSONNode.tsx
@@ -1,24 +1,24 @@
 import * as React from "react";
 import { Node } from "../utils/ToTree";
+import { observer } from "mobx-react";
 
-React.Component;
 
-export function JSONNode(props: JSONNodeProps) {
-  return (
-    <div>
-      <div className={"element"}>
-        <div
-          onClick={() => props.node.params.expanded = !props.node.params.expanded}
-          className={"expander"}
-        >+</div>
-        <div className={"name"}>{props.node.params.id}</div>
+export const JSONNode = observer((props: JSONNodeProps) => (
+  <div>
+    <div className={"element"}>
+      <div
+        onClick={() => props.node.params.expanded = !props.node.params.expanded}
+        className={"expander"}
+      >+
       </div>
-      <div className={"elements"}>
-        {props.node.params.enumerable && (props.node.value as Node[]).map(node => <JSONNode node={node}/>)}
-      </div>
+      <div className={"name"}>{props.node.params.id}</div>
     </div>
-  );
-}
+    <div className={"elements"}>
+      {props.node.params.enumerable && (props.node.value as Node[]).map(node => <JSONNode key={props.node.params.id}
+                                                                                          node={node}/>)}
+    </div>
+  </div>
+));
 
 interface JSONNodeProps {
   node: Node
